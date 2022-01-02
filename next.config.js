6f246a1add0086720d1f7f1b5be0d544779b59a3
yoshiiki_shino <yoshiki.shino.tech@gmail.com>
2022-01-02 21:51:02 +0900

    chore: deploy pipeline

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: process.env.BASE_PATH || '',
+  basePath: process.env.BASE_PATH || '',
 }
