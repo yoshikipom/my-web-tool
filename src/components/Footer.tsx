4d88795a81bad7f7330c9de866a3761f84f4127a
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2022-01-03 21:12:12 +0900

    feat: link to tool

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            © 2022 Yoshiki Shino.
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/pages/java-stacktrace.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link';
+
+const JavaStacktrace: NextPage = () => {
+    return (
+        <div>
+            java stacktrace
+        </div>
+    )
+}
+
+export default JavaStacktrace
